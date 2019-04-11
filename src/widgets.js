bde2c860311db7a4e38a18902e3fdf6bb55f6878
Patrick Altman <paltman@gmail.com>
2019-04-11 13:35:55 -0500

    Remove widgets that don't apply to homer

--- src/widgets.js
+++ src/widgets.js
@@ -1,18 +1,16 @@
-import HomerWidget from './components/HomerWidget.vue';
 import HomerReaderWidget from './components/HomerReaderWidget.vue';
 import HomerGridReaderWidget from './components/HomerGridReaderWidget.vue';
 import HomerParallelReaderWidget from './components/HomerParallelReaderWidget.vue';
 import HomerReferenceInputWidget from './components/HomerReferenceInputWidget.vue';
-import ReaderWidget from './reader/ReaderWidget.vue';
 import SelectedTextInfoWidget from './reader/SelectedTextInfoWidget.vue';
 import CTSWidget from './reader/CTSWidget.vue';
+import CardReaderWidget from './reader/CardReaderWidget.vue';
 import TextSizeWidget from './text-size/TextSizeWidget.vue';
 
 export default {
-  [HomerWidget.scaifeConfig.displayName]: HomerWidget,
-  [ReaderWidget.scaifeConfig.displayName]: ReaderWidget,
   [HomerReaderWidget.scaifeConfig.displayName]: HomerReaderWidget,
   [HomerGridReaderWidget.scaifeConfig.displayName]: HomerGridReaderWidget,
+  [CardReaderWidget.scaifeConfig.displayName]: CardReaderWidget,
   [HomerParallelReaderWidget.scaifeConfig.displayName]: HomerParallelReaderWidget,
   [TextSizeWidget.scaifeConfig.displayName]: TextSizeWidget,
   [SelectedTextInfoWidget.scaifeConfig.displayName]: SelectedTextInfoWidget,
