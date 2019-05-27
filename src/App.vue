12379810145f6deaad60f98685238f32e4e2b268
Patrick Altman <paltman@gmail.com>
2019-05-27 13:00:41 -0500

    Integrate with new-style scaife skeleton

--- src/App.vue
+++ src/App.vue
@@ -1,81 +1,13 @@
 <template>
-  <EditableSkeleton
-      id="app"
-      @leftToggle="onLeftToggle"
-      @rightToggle="onRightToggle"
-      @addWidget="addWidget"
-      @changeWidget="changeWidget"
-      @removeWidget="removeWidget"
-      :left-open="leftOpen"
-      :right-open="rightOpen"
-      :main-widget="mainWidget"
-      :left-widgets="leftWidgets"
-      :right-widgets="rightWidgets"
-  />
+  <EditableSkeleton id="app" />
 </template>
 
 <script>
 import EditableSkeleton from './skeleton/EditableSkeleton.vue';
-import {
-  TOGGLE_RIGHT_SIDEBAR,
-  TOGGLE_LEFT_SIDEBAR,
-  ADD_LEFT_WIDGET,
-  ADD_RIGHT_WIDGET,
-  CHANGE_MAIN_WIDGET,
-  REMOVE_LEFT_WIDGET,
-  REMOVE_RIGHT_WIDGET,
-} from './constants';
-import widgets from './widgets';
 
 export default {
-  name: 'app',
   components: {
     EditableSkeleton,
-  },
-  methods: {
-    onLeftToggle() {
-      this.$store.dispatch(TOGGLE_LEFT_SIDEBAR);
-    },
-    onRightToggle() {
-      this.$store.dispatch(TOGGLE_RIGHT_SIDEBAR);
-    },
-    addWidget(name, widget) {
-      if (name === 'left') {
-        this.$store.dispatch(ADD_LEFT_WIDGET, { displayName: widget.scaifeConfig.displayName });
-      } else {
-        this.$store.dispatch(ADD_RIGHT_WIDGET, { displayName: widget.scaifeConfig.displayName });
-      }
-    },
-    changeWidget(mainWidget) {
-      this.$store.dispatch(CHANGE_MAIN_WIDGET, { displayName: mainWidget.scaifeConfig.displayName });
-    },
-    removeWidget(name, index) {
-      if (name === 'left') {
-        this.$store.dispatch(REMOVE_LEFT_WIDGET, { index });
-      } else {
-        this.$store.dispatch(REMOVE_RIGHT_WIDGET, { index });
-      }
-    },
-  },
-  computed: {
-    widgets() {
-      return widgets;
-    },
-    mainWidget() {
-      return widgets[this.$store.state.widgets.mainWidget] || null;
-    },
-    leftWidgets() {
-      return this.$store.state.widgets.left.filter(w => widgets[w] !== undefined).map(w => widgets[w]);
-    },
-    rightWidgets() {
-      return this.$store.state.widgets.right.filter(w => widgets[w] !== undefined).map(w => widgets[w]);
-    },
-    leftOpen() {
-      return this.$store.state.leftOpen;
-    },
-    rightOpen() {
-      return this.$store.state.rightOpen;
-    },
   },
 };
 </script>
--- src/constants.js
+++ src/constants.js
@@ -1,12 +1,3 @@
-// Skeleton
-export const TOGGLE_LEFT_SIDEBAR = 'toggleLeftSidebar';
-export const TOGGLE_RIGHT_SIDEBAR = 'toggleRightSidebar';
-export const ADD_LEFT_WIDGET = 'addLeftWidget';
-export const ADD_RIGHT_WIDGET = 'addRightWidget';
-export const CHANGE_MAIN_WIDGET = 'changeMainWidget';
-export const REMOVE_LEFT_WIDGET = 'removeLeftWidget';
-export const REMOVE_RIGHT_WIDGET = 'removeRightWidget';
-
 // Reader
 export const PREVIOUS_CARD = 'previousCard';
 export const NEXT_CARD = 'nextCard';
