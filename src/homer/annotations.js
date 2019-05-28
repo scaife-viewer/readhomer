22c2e98d2b12f36da0e6e5273d78df8e09c15ac4
Patrick Altman <paltman@gmail.com>
2019-05-28 11:18:26 -0500

    Add a sample of token translation alignment

---
+++ src/homer/annotations.js
@@ -0,0 +1,23 @@
+const annotations = {
+  iliad: [
+    {
+      kind: 'translation',
+      greek: [
+        { chapter: 1, line: 1, offsets: [0] },
+        { chapter: 1, line: 1, offsets: [1] },
+        { chapter: 1, line: 1, offsets: [2] },
+        { chapter: 1, line: 1, offsets: [3] },
+        { chapter: 1, line: 1, offsets: [4] },
+      ],
+      english: [
+        { chapter: 1, line: 1, offsets: [0, 1] },
+        { chapter: 1, line: 1, offsets: [2] },
+        { chapter: 1, line: 1, offsets: [3] },
+        { chapter: 1, line: 1, offsets: [4, 5, 6] },
+        { chapter: 1, line: 1, offsets: [7] },
+      ],
+    },
+  ],
+};
+
+export default annotations;
