efbf7e993e01baeb6c935adc55eb4759b2cc1a8c
Ralph Giles <giles@thaumas.net>
2019-07-29 10:01:50 -0700

    Restore explicit loading of global components.
    Patrick Altman prefers an explicit export list rather than
    loading everything in the directory.
    
    Restore global-components/index.js with an explicit list of
    default exports, which was removed in the previous commit,
    and revert to Jacob Wegner's loading solution from commit
    16f106527bef04893ecfbd94b64808f5fdd323e1.
    
    Instead of deriving the component name from the debug-only
    __file property, export the desired components as properties
    of the default object and use Object.entries to enumerate over
    them, using the property name, and hence module import name, as the
    registered component name.

---
+++ src/global-components/index.js
@@ -0,0 +1,7 @@
+import Icon from './Icon.vue';
+import TextLoader from './TextLoader.vue';
+
+export default {
+  Icon,
+  TextLoader,
+};
--- src/main.js
+++ src/main.js
@@ -2,18 +2,15 @@ import Vue from 'vue';
 import App from './App.vue';
 import store from './store';
 
+import globalComponents from './global-components';
 import SkeletonPlugin from './skeleton/plugin';
 import widgets from './widgets';
 
 Vue.config.productionTip = false;
 
-// Register what's in the global components directory.
-const globalComponents = require.context('./global-components', true, /\.vue$/i);
-
-globalComponents.keys().forEach((path) => {
-  // Component's filename without the extension.
-  const name = path.split('/').pop().split('.')[0];
-  Vue.component(name, globalComponents(path).default);
+// Register exported global components.
+Object.entries(globalComponents).forEach(([name, component]) => {
+  Vue.component(name, component);
 });
 
 Vue.use(SkeletonPlugin, { widgets });
